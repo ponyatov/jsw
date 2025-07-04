f1b44cfaa75a588fe73addb909d59b0bad5c75b4
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-04 22:44:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ mk/src.mk
@@ -0,0 +1,10 @@
+# .mk files
+MK += Makefile
+MK += $(wildcard   mk/*.mk)
+MK += $(wildcard   hw/*.mk)
+MK += $(wildcard  cpu/*.mk)
+MK += $(wildcard arch/*.mk)
+MK += $(wildcard   os/*.mk)
+
+# JavaScript
+J += $(wildcard src/*.js)
