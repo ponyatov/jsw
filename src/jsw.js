9b0e2ac3c8ad2fa9bee23182396e5668bde651de
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-04 23:57:04 +0400

    .

--- src/jsw.js
+++ src/jsw.js
@@ -1,1 +1,11 @@
 console.log(1 + 2 / 3);
+
+const libjsw = Deno.dlopen(
+    './target/x86_64-unknown-linux-gnu/debug/libjsw.so',
+    {
+        add_numbers: { parameters: ['i32', 'i32'], result: 'i32' },
+    }
+);
+
+libjsw.symbols.add_numbers(12, 34);
+console.log(libjsw);
